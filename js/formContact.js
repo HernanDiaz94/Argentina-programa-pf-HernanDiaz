0f744f83271517e94e16180b5f88edc5dd92cc40
HernanDiaz94 <hernan.diiaz@gmail.com>
2023-02-26 18:55:56 -0300

    se agrego funcionalidad para anviar email desde form de contacto

---
+++ js/formContact.js
@@ -0,0 +1,16 @@
+//$Nombrevariable es por que es un elemento del DOM
+const $form = document.querySelector('#form')
+const $buttonMailTo = document.querySelector('#emailSend')
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault();
+    const form = new FormData(this);
+    console.log(form.get('nombre'));
+
+    $buttonMailTo.setAttribute('href', 
+    `mailto:hernan.diiaz@gmail.com
+    ?subject=${form.get('nombre')} ${form.get('email')}
+    &body=${form.get('consulta')}`);
+    $buttonMailTo.click();
+}
--- js/prueba.js
+++
@@ -1,8 +0,0 @@
-console.log("Hola mundo");
-
-let numero1 = 3;
-let numero2 = 4;
-
-let resultado = numero1+numero2;
-
-console.log("La suma de numero1 y numero2 es: "+ resultado);
